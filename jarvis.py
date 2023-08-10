477b4eed89cb0373891fde03f6ade7ccc6f832cf
Shyam Sundar Singh <sssinghyadav4802@gmail.com>
2023-08-11 02:38:32 +0530

    Added Other instruction for AI Desktop voice Assistant

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,9 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia
+import webbrowser
+import os
+import random
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
@@ -35,11 +38,6 @@ def takeCommand():
     return query
 
 
-
-    
-
-
-
 if __name__ == "__main__" :
     wishMe()
     while True:
@@ -51,6 +49,35 @@ if __name__ == "__main__" :
             speak("According to wikipedia")
             print(results.encode('utf-8'))
             speak(results)
+        elif "open youtube" in query:
+            webbrowser.open("https://www.youtube.com/")
+        elif "open google" in query:
+            webbrowser.open("https://www.google.com/")
+        elif "open netflix" in query:
+            webbrowser.open("https://netflix-clone-b9b3e.web.app")
+        elif 'play music' in query:
+            music_dir='D:\\Music-Player' 
+            songs=os.listdir(music_dir)
+            print(songs)
+            random_song=random.choice(songs)
+            os.startfile(os.path.join(music_dir,random_song))
+        elif "the time" in query:
+            strTime =datetime.datetime.now().strftime('%H:%M:%S')
+            print(f"Generated response: Shyam, the time is {strTime}")
+            speak(f"Shyam, the time is {strTime}")
+        elif "open chrome" in query:
+            browser="C:\\Users\\Public\\Desktop\\Google Chrome.lnk"
+            os.startfile(browser)
+
+        
+
+
+        
+
+
+
+
+
 
 
 
